c86f337997c06321158e6d9eecb4c7850030d905
YorhaJ <146982938+YorhaJ@users.noreply.github.com>
2023-10-04 18:03:31 +0100

    Add files via upload

---
+++ Portfolio_project_COVID.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations_smoothed]
+      ,[RollingPeopleVaccinated]
+  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
